a1218099623be77158caae7bb75f8ae45cda9133
frsargua <frsargua@gmail.com>
2022-05-14 20:06:20 +0100

    updated version of onclick function for dynamic NB

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 // Elements
 const searchBarEl = $("#searchbar-dynamic");
 const dropDownTitles = $("#dropdown-menu-titles");
+const dropDownContainerEl = $("#dropdown-menu");
 
 // This fetches the titles from the IMD api for the dynamic dropdown list
 
 // Render options
+const renderDropDownMenu = () => {
+  console.log("rendering dropdown");
+  const dropdownMenu = `<div class="dropdown-content" id="dropdown-menu-titles"></div>`;
+  dropDownContainerEl.append(dropdownMenu);
+};
 const renderResults = (title) => {
+  const dropDownTitles = $("#dropdown-menu-titles");
   const searchResult = `<a href="#" class="dropdown-item">
                         ${title}
                     </a>`;
@@ -14,9 +21,10 @@ const renderResults = (title) => {
 
 // Onclick button
 searchBarEl[0].addEventListener("keypress", function (event) {
+  dropDownContainerEl.empty();
+  dropDownTitles.empty();
+  renderDropDownMenu();
   if (event.keyCode === 13) {
-    dropDownTitles.empty();
-
     const title = searchBarEl.val();
     const url = `https://imdb-api.com/en/API/SearchMovie/k_voxajyfz/${title}`;
     fetch(url)
