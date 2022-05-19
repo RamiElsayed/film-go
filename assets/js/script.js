0df6a9f87d1eeb826ccc339cc46b8cfae82c5643
Sachin <sachin.basandrai@mwphillips.co.uk>
2022-05-19 21:55:18 +0100

    adjusted for different screen sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,10 +115,11 @@ window.onload = function () {
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
   document.getElementById("mySidepanel").style.width = "350px";
-  document.getElementById("mySidepanel").style.height = "50%";
+  document.getElementById("mySidepanel").style.height = "100%";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidepanel").style.height = "0";
 }
