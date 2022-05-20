f2c556c865b159333aaad8f4b327a9c72caa3d35
Rami Elsayed <ramielsayed123@outlook.com>
2022-05-20 13:17:20 +0100

    deleted from main

--- assets/js/top10.js
+++ assets/js/top10.js
@@ -1,20 +1,4 @@
-const genresListApi = `https://api.themoviedb.org/3/genre/movie/list?api_key=7c7537b799513b436eb6bed714d7edcc&language=en-US`;
 
-const getGenreId= async (url) => {
-    const response = await fetch(url);
-    const data = await (response.json());
-    return data;
-}
-
-const matchGenreId = (genre) => {
-    const data = getGenreId(url);
-
-    for (let index = 0; index < data.length; index++) {
-       if (data[index].name === genre) {
-           return data[index].id;
-       }
-    }
-}
 
 
 
