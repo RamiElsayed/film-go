8b853d5a7fec6c3010607a300ff5637dce87e727
Rami Elsayed <ramielsayed123@outlook.com>
2022-05-20 16:30:58 +0100

    fetchin APi and getting ID

--- assets/js/top10.js
+++ assets/js/top10.js
@@ -1,4 +1,19 @@
+const genresListApi = `https://api.themoviedb.org/3/genre/movie/list?api_key=7c7537b799513b436eb6bed714d7edcc&language=en-US`;
 
+const getGenreId= async (genresListApi) => {
+    const response = await fetch(genresListApi);
+    const data = await (response.json());
+    return data;
+}
+
+const matchGenreId = (genre) => {
+    const data = getGenreId(genresListApi);
+    for (let index = 0; index < data.length; index++) {
+       if (data[index].name === genre) {
+           return data[index].id;
+       }
+    }
+}
 
 
 
