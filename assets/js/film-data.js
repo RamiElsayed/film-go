169b48395a99463e62baf89a74f7ff9218a4eb2d
frsargua <frsargua@gmail.com>
2022-05-21 10:34:34 +0100

    Adding thumbnail-sizing class to style a tags

--- assets/js/film-data.js
+++ assets/js/film-data.js
@@ -2,24 +2,25 @@ const mainElem = document.querySelector("main");
 const videosListElem = document.getElementById("videos-container");
 
 const embedYoutubeVideos = (searchTerm) => {
-    function searchYoutubeVideos(searchTerm) {
-        return fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&key=AIzaSyBhTjmkiNU9nfU2EIRNhCQl6R9RYNSf8wc`
-        );
-    };
+  function searchYoutubeVideos(searchTerm) {
+    return fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&key=AIzaSyBhTjmkiNU9nfU2EIRNhCQl6R9RYNSf8wc`
+    );
+  }
 
-    function linkYouTubeVideo(videoId) {
-        return `https://www.youtube.com/watch?v=${videoId}`;
-    };
+  function linkYouTubeVideo(videoId) {
+    return `https://www.youtube.com/watch?v=${videoId}`;
+  }
 
-    searchYoutubeVideos(searchTerm)
-    .then(response => response.json())
-    .then(data => {
-        console.log(`youtube data: ${data.items.length}`);
-        for (let i = 0; i < 5; i++) {
+  searchYoutubeVideos(searchTerm)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`youtube data: ${data.items.length}`);
+      for (let i = 0; i < 5; i++) {
         const videoId = data.items[i].id.videoId;
         const filmYoutubeVideo = document.createElement("a");
-        filmYoutubeVideo.setAttribute("target","_blank");
+        filmYoutubeVideo.setAttribute("target", "_blank");
+        filmYoutubeVideo.setAttribute("class", "thumbnail-sizing");
         filmYoutubeVideo.href = linkYouTubeVideo(videoId);
 
         const filmImage = document.createElement("img");
@@ -27,8 +28,8 @@ const embedYoutubeVideos = (searchTerm) => {
 
         filmYoutubeVideo.appendChild(filmImage);
         videosListElem.append(filmYoutubeVideo);
-        }
+      }
     });
-}
+};
 
 embedYoutubeVideos("avatar");
